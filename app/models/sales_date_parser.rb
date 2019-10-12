75e9476f0fc5732b0310b1b68ffc270dcbc96d26
nogtk <taka.naoga@moneyforward.co.jp>
2019-10-12 17:35:26 +0900

    Fix regexp

--- app/models/sales_date_parser.rb
+++ app/models/sales_date_parser.rb
@@ -42,7 +42,7 @@ class SalesDateParser
   end
 
   def get_date_from_content_by_regexp(content)
-    match_data = content.match(/(\d{1, 2})\/(\d{1, 2})/) || content.match(/(\d{1, 2})月(\d{1, 2})日/)
+    match_data = content.match(/(\d{1,2})\/(\d{1,2})/) || content.match(/(\d{1,2})月(\d{1,2})日/)
     if match_data
       Date.new(Date.current.year, match_data[1].to_i, match_data[2].to_i)
     end
