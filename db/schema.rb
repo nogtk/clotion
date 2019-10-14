2ee2a728a8366542a0482a3f720bae3ee93cdb0c
nogtk <taka.naoga@moneyforward.co.jp>
2019-10-14 12:50:03 +0900

    Add twitter_user_id to Shop model for search tweet of each store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_123525) do
+ActiveRecord::Schema.define(version: 2019_10_14_033120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_10_12_123525) do
     t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "twitter_user_id", null: false
   end
 
 end
--- lib/tasks/fetch_sales_info.rake
+++ lib/tasks/fetch_sales_info.rake
@@ -2,9 +2,9 @@ namespace :fetch_sales_info do
   desc "Fetch stores to aggregate by using Twitter API"
   task fetch_stores: :environment do
     Shop.destroy_all
-    @client = TwitterClient.new
-    @client.fetch_shop_from_list.each do |shop|
-      s = Shop.new(name: shop.name, url: shop.to_h[:url])
+    client = TwitterClient.new
+    client.fetch_shop_from_list.each do |shop|
+      s = Shop.new(name: shop.name, url: shop.to_h[:url], twitter_user_id: shop.id)
       s.save!
     end
   end
