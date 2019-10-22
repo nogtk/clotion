b69025f425f4d14c5e99d61d563ff8970bd8a70b
nogtk <taka.naoga@moneyforward.co.jp>
2019-10-22 15:35:58 +0900

    Enable config.assets.compile is true in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+
+  # Raise error when execution on heroku environment.
+  # So, Chenged config.assets.compile = true
+  # ref: https://qiita.com/pugiemonn/items/1663ca88766ceba6a397
+  # config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
