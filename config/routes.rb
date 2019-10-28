c52c639f29ff59dc82ad6a8f8325fa604419d967
nogtk <taka.naoga@moneyforward.co.jp>
2019-10-28 23:34:52 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'twitter_clients#index'
   resources :twitter_clients, only: [:index]
   resources :about, only: [:index]
 end
