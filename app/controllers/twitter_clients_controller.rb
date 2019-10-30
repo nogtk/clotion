dcaff25be7f8f39c37a9e5f73fc066b2972f44ad
nogtk <taka.naoga@moneyforward.co.jp>
2019-10-30 21:20:49 +0900

    Show twitter images

--- app/controllers/twitter_clients_controller.rb
+++ app/controllers/twitter_clients_controller.rb
@@ -1,5 +1,5 @@
 class TwitterClientsController < ApplicationController
   def index
-    @sales_info = SaleInfo.all.includes(:shop)
+    @sales_info = SaleInfo.all.includes(shop: :images)
   end
 end
